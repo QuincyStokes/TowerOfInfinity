a1c0bd873fc2540534cfb629c0a9aa5ce6aeed5a
the gamer <133170486+a01username@users.noreply.github.com>
2025-05-03 13:53:49 -0700

    FInal version2.0

--- Assets/Scripts/TilemapSetup.cs
+++ Assets/Scripts/TilemapSetup.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.Collections;
 using UnityEditor;
-using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 public class TilemapSetup : MonoBehaviour
