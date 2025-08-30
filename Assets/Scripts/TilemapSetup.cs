5cd140a3ed084f764a19c0f1c37be47018b8ee39
the gamer <lx080418@gmail.com>
2025-08-30 13:29:19 -0700

    Changed Boss position

--- Assets/Scripts/TilemapSetup.cs
+++ Assets/Scripts/TilemapSetup.cs
@@ -330,7 +330,7 @@ public class TilemapSetup : MonoBehaviour
     private void GenerateBossRoom(int x, int y, int level)
     {
         GameObject bossRoom = Instantiate(level1BossRoom, new Vector2(x * roomWidth-1, y * roomWidth), Quaternion.identity, mainGrid.transform);
-        GameObject boss = Instantiate(level1Boss, new Vector2(x * roomWidth + roomWidth/2, y * roomWidth + roomWidth/2), Quaternion.identity);
+        GameObject boss = Instantiate(level1Boss, new Vector2(x * roomWidth + roomWidth/2, y * roomWidth + roomWidth), Quaternion.identity);
 
         string bossHealth;
         switch(level){
